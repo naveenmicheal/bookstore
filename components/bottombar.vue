181482da69d9cc00d7a78ab928adc3e85117de5e
zero <naveenmichael.alpha.local>
2020-10-31 16:43:46 +0530

    fix

--- components/bottombar.vue
+++ components/bottombar.vue
@@ -2,7 +2,7 @@
     <div class="">
         <b-navbar v-if="cartproducts.length >0" id="navbar" shadow fixed-bottom :mobile-burger=bar>
             <template slot="brand">  
-             <b-button tag="nuxt-link" to="/checkout" @click="" type="is-link is-light" size="is-    " icon-right="cart" expanded style="border-radius: 0" >
+             <b-button tag="nuxt-link" to="/cart" @click="" type="is-link is-light" size="is-    " icon-right="cart" expanded style="border-radius: 0" >
                 Go to Cart
             </b-button>
         </template>
